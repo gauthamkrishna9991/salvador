e0fa4e758c171755ad41e57dc4f9f4ba02ef4d21
Goutham Krishna K V <gauthamkrishna9991@live.com>
2020-12-15 22:36:48 +0530

    add some comments

--- src/lib/dynarr.h
+++ src/lib/dynarr.h
@@ -13,14 +13,19 @@ typedef struct __dynarr {
     size_t capacity;
 } dynarr;
 
+// create a dynamic array
 dynarr* dynarr_create();
 
+// insert an element of type integer to the dynamic array
 dynarr* dynarr_insert(dynarr* arr, int element);
 
+// give them the pointer to dynamic array
 int *dynarr_getarr(dynarr* arr);
 
+// get the length of the dynamic array
 size_t dynarr_getlength(dynarr *arr);
 
+// get the capacity (allocated size, NOT length) of the dynamic array
 size_t dynarr_getcapacity(dynarr *arr);
 
 #endif
